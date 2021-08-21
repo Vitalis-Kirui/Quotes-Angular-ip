784e851971fda9580eef05e553fb88bfb6cbfed6
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-08-21 12:32:59 +0300

    pushing two data binding on the form and styles on the form

--- src/app/qoutes-form/qoutes-form.component.ts
+++ src/app/qoutes-form/qoutes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuotesConstructor } from '../quotes-constructor';
 
 @Component({
   selector: 'app-qoutes-form',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qoutes-form.component.css']
 })
 export class QoutesFormComponent implements OnInit {
+
+  newQuote = new QuotesConstructor (0, "", "", "");
 
   constructor() { }
 
