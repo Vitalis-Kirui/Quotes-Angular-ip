a350ed576e5540be95dbe4c04da0a9fa2ac1877c
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-08-21 10:56:14 +0300

    pushing fully functioning highligther directive.It puts a background color to the object

--- src/app/highlighter-directive.directive.ts
+++ src/app/highlighter-directive.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlighterDirective]'
 })
 export class HighlighterDirectiveDirective {
 
-  constructor() { }
+  constructor( private elem : ElementRef) { 
+
+    // this.elem.nativeElement.style.backgroundColor = 'tomato';
+
+  };
+
+  @HostListener ("click") onClicks (){
+
+    this.highligther("tomato")
+
+  };
+
+  @HostListener ("dblclick") onDoubleClicks(){
+
+    this.highligther("")
+
+  };
+
+  private highligther( action : string){
+
+    this.elem.nativeElement.style.backgroundColor = action;
+
+  };
 
 }
