d212cf671229a80e81c8b049781162a7de9b266d
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-08-21 08:27:28 +0300

    pushing constructor parameter and array of Qoutes.

--- src/app/qoutes-parent/qoutes-parent.component.ts
+++ src/app/qoutes-parent/qoutes-parent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuotesConstructor } from '../quotes-constructor';
 
 @Component({
   selector: 'app-qoutes-parent',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qoutes-parent.component.css']
 })
 export class QoutesParentComponent implements OnInit {
+
+  appTitle = "Qoutes Central";
+
+  introductionText = "Brigthen someone's face today with your favorite Quote.";
+
+  quotesArray: QuotesConstructor [] = [
+
+    new QuotesConstructor(1, "Live as if you were to die tomorrow. Learn as if you were to live forever.", "Mahatma Gandhi", "Kipyegon Kirui Vitalis"),
+    new QuotesConstructor(2, "If you cannot do great things, do small things in a great way.", "Napoleon Hill", "Hrithik Roshan"),
+    new QuotesConstructor(3, "Wise men speak because they have something to say; fools because they have to say something.", "Plato", "Mercy Daniel")
+
+  ];
 
   constructor() { }
 
--- src/app/quotes-constructor.ts
+++ src/app/quotes-constructor.ts
@@ -2,7 +2,7 @@ export class QuotesConstructor {
 
     showDetails : boolean;
 
-    constructor (public details : string){
+    constructor(public id: number, public quote: string, public authorName: string, public details: string ){
 
         this.showDetails = false;
 
