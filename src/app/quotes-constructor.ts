65a23fc47a23c676abd933a948aae92618c4265e
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-08-21 09:03:46 +0300

    pushing html code for display array of quotes and their css styles

--- src/app/quotes-constructor.ts
+++ src/app/quotes-constructor.ts
@@ -2,7 +2,7 @@ export class QuotesConstructor {
 
     showDetails : boolean;
 
-    constructor(public id: number, public quote: string, public authorName: string, public details: string ){
+    constructor(public id: number, public quotes: string, public authorName: string, public details: string ){
 
         this.showDetails = false;
 
